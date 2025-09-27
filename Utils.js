bd95eb7977c0e854ea11d0822915ea77e268bf3c
Rust <rudy_curioso@outlook.com>
2025-09-27 09:49:38 -0600

    Api Key

---
+++ Utils.js
@@ -0,0 +1,19 @@
+// utils.js
+
+const API_KEY = 'i3fB518bc4HWgErQH5r7oz6W3vAwepfb8sTevDZ6'; 
+
+
+async function getApod() {
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Error en la respuesta de la red: ${response.statusText}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error al obtener APOD:', error);
+        return null; 
+    }
+}
